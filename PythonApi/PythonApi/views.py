79331c98f5fad4ec9f2f73271d2205a1ed170418
apoorva sapra <sapra.apoorva88@gmail.com>
2022-01-06 22:22:50 +0530

    Testing views.py

--- PythonApi/PythonApi/views.py
+++ PythonApi/PythonApi/views.py
@@ -17,9 +17,10 @@ def external(request):
     fileurl=ds.open(filename)
     templateurl=ds.url(filename)
  
-    gifSavePath= CreateGif.createGif(str(fileurl),str(filename),name)
+    # gifSavePath= CreateGif.createGif(str(fileurl),str(filename),name)
 
-    gif_name=os.path.basename(gifSavePath)
+    # gif_name=os.path.basename(gifSavePath)
+    gif_name="temp.gif"
 
     gif_url=ds.url(gif_name)
     return render(request,'home.html',{'raw_url':templateurl,'edit_url':gif_url})
