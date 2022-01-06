d653e5108fb2eb05f3e2307f241e09e195ec0a57
apoorva sapra <sapra.apoorva88@gmail.com>
2022-01-06 11:42:08 +0530

    reove WEBSITE_ENABLE_SYNC_UPDATE_SITE = false

--- PythonApi/PythonApi/settings.py
+++ PythonApi/PythonApi/settings.py
@@ -3,7 +3,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-WEBSITE_ENABLE_SYNC_UPDATE_SITE = false
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
