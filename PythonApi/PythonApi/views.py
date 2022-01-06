a8908df368831323e6824713afd6b9609bb3ed2a
apoorva sapra <sapra.apoorva88@gmail.com>
2022-01-06 22:40:03 +0530

    Revert "Testing views.py"
    This reverts commit 79331c98f5fad4ec9f2f73271d2205a1ed170418.

--- PythonApi/PythonApi/views.py
+++ PythonApi/PythonApi/views.py
@@ -17,10 +17,9 @@ def external(request):
     fileurl=ds.open(filename)
     templateurl=ds.url(filename)
  
-    # gifSavePath= CreateGif.createGif(str(fileurl),str(filename),name)
+    gifSavePath= CreateGif.createGif(str(fileurl),str(filename),name)
 
-    # gif_name=os.path.basename(gifSavePath)
-    gif_name="temp.gif"
+    gif_name=os.path.basename(gifSavePath)
 
     gif_url=ds.url(gif_name)
     return render(request,'home.html',{'raw_url':templateurl,'edit_url':gif_url})
