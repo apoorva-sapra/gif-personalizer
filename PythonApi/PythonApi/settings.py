8119be430e72a46a8b4733216c10e2a517e6d167
apoorva sapra <sapra.apoorva88@gmail.com>
2022-01-17 09:44:19 +0530

    Added django cors in requirements and script for inserting content in outlook mail

--- PythonApi/PythonApi/settings.py
+++ PythonApi/PythonApi/settings.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 import os
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = (
+    'GET',
+    'POST',
+    'OPTIONS',
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
